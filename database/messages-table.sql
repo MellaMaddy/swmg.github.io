c20da37eb5f55029c3af605380cdee030873d577
SDennismark <sarahdenmark1@gmail.com>
2025-11-11 19:41:21 -0600

    Delete database/messages-table.sql

--- database/messages-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS messages (
-    message_id SERIAL PRIMARY KEY,
-    sender_id INTEGER NOT NULL,
-    receiver_id INTEGER NOT NULL,
-    encrypted_message TEXT NOT NULL,
-    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (sender_id) REFERENCES users(id),
-    FOREIGN KEY (receiver_id) REFERENCES users(id)
-);
