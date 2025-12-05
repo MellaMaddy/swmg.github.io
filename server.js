45f486e02f14cf7c704610f9f57dc0e52d8d5a56
MellaMaddy <168486185+MellaMaddy@users.noreply.github.com>
2025-12-04 20:44:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -110,7 +110,3 @@ io.on("connection", (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
